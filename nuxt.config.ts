52a18175972a2eb3a2496878fb71391a82fe9a5e
MrAnyx <1ptitigre@gmail.com>
2024-10-19 16:31:40 +0000

    feat(core) Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  extends: ['shadcn-docs-nuxt'],
+  compatibilityDate: '2024-07-06',
+  devServer: {
+    port: 3001
+  }
+});
